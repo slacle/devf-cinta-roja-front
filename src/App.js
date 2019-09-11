5167eb34b0c1e281904565f3f4c2e4732b93e952
Sebastian Laclé <info@slacle.com>
2019-09-11 16:23:43 -0500

    changed main folder to have front

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Item from "./components/Item";
 import Form from "./components/Form";
 // import Main from "./components/Main";
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       items: []
     };
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,25 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Item extends Component {
-  render() {
-    return (
-      <form onSubmit={this.props.handleSubmit}>
-        <div className="form-group">
-          <label>Name</label>
-          <input type="text" className="form-control" name="name" />
-        </div>
-        <div className="form-group">
-          <label>Description</label>
-          <input type="text" className="form-control" name="description" />
-        </div>
-        <div className="form-group">
-          <label>Photo</label>
-          <input type="text" className="form-control" name="photo" />
-        </div>
-        <button className="btn btn-primary">Submit</button>
-      </form>
-    );
-  }
-}
+const Item = props => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div className="form-group">
+        <label>Name</label>
+        <input type="text" className="form-control" name="name" />
+      </div>
+      <div className="form-group">
+        <label>Description</label>
+        <input type="text" className="form-control" name="description" />
+      </div>
+      <div className="form-group">
+        <label>Photo</label>
+        <input type="text" className="form-control" name="photo" />
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  );
+};
 
 export default Item;
